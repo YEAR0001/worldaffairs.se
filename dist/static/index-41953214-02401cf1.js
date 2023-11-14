de67a1eac3862a34d24e0a2c974b35219ee94437
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-11-14 15:37:37 +0100

    update sanity

---
+++ dist/static/index-41953214-02401cf1.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-1873ab3b-fbbbc044.js";import"./sanity-1231ba1e.js";import"./json-inspector-dd30324f.js";export{m as DocumentPaneProvider,a as default};
